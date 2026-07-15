f1a05665256dd394c4193841c76f4fa3dfee9aca
ClaRave28 <clarisse15032004@gmail.com>
2026-07-15 11:51:47 +0200

    mise en place du syllabus création d'un fichier inspirations.md

--- src/components/sections/AboutMe/AboutMe.tsx
+++ src/components/sections/AboutMe/AboutMe.tsx
@@ -3,9 +3,25 @@ import imageMe from '@/assets/me.jpeg'
 import FlowerBox from '@/components/Box/FlowerBox';
 import FlowerBoxMe from '@/components/Box/FlowerBoxMe';
 import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Sound } from "@/Hooks/Sound"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 
 function AboutMe() {
+    const playClick = Sound("/sounds/clic.mp3");
     return (
         <div className="flex flex-col">
 
@@ -25,7 +41,7 @@ function AboutMe() {
                         <div className="relative -mt-[30px] -ml-[400px] w-80 h-80 mx-auto animate-slide-in-left">
                             <FlowerBoxMe className="absolute inset-0 w-full h-full" image={imageMe} />
 
-                            
+
                         </div>
 
                         <div className="relative -mt-[30px] -ml-[1px] w-80 h-80 mx-auto animate-slide-in-top">
@@ -60,13 +76,171 @@ function AboutMe() {
                             <div className="w-48 text-center text-lilac-100 text-xl leading-snug">
                                 Digital and Intelligent Systems
                             </div>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <Button
+                                        onClick={() => {
+                                            playClick();
+                                        }}
+                                        className=" btn-glossy bg-lilac-950 text-lilac-100  transition-all duration-300 hover:drop-shadow-[0_0_20px_var(--color-lilac-400)]">
+                                        Syllabus
+                                    </Button>
+                                </DialogTrigger>
 
-                            <Button
+                                <DialogContent className="bg-[#1a1025] text-white border-[var(--lavender-purple)] max-h-[80vh] overflow-y-auto sm:max-w-[1150px]">
+                                    <DialogHeader>
+                                        <DialogTitle className="text-[var(--lavender-purple)]">
+                                            Syllabus des cours
+                                        </DialogTitle>
 
-                                className=" border p-1 rounded-xl bg-lilac-500 text-lilac-100 text-s font-born transition-all duration-300 hover:drop-shadow-[0_0_20px_var(--color-lilac-400)]">
+                                    </DialogHeader>
 
-                                Syllabus
-                            </Button>
+                                    <p className="text-sm text-[var(--white)]/70">
+                                        4th year
+                                    </p>
+
+                                    <div className="flex gap-4">
+                                        <div className="flex-column">
+                                            <Accordion type="single" collapsible className="w-full">
+
+
+
+                                                <AccordionItem value="labour-law" className="border-lilac-800">
+                                                    <AccordionTrigger>
+                                                        Labour Law
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Droit du travail, loi sur le travail...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="java" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Java Programming Language
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="mobile-materials" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Mobile Materials
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+
+                                            </Accordion>
+                                        </div>
+                                        <div className="flex-column">
+                                            <Accordion type="single" collapsible className="w-full">
+
+                                                <AccordionItem value="angular-js" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Angular and JS
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="symfony" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Symfony Web Development
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="unix-linux" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        UNIX and LINUX Systems
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+                                            </Accordion>
+
+                                        </div>
+
+                                        <div className="flex-column">
+                                            <Accordion type="single" collapsible className="w-full">
+
+                                                <AccordionItem value="git" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Git and Version Control
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="apis" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        API's Architecture
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="networks" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Networks and Infrastructure
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                            </Accordion>
+
+                                        </div>
+
+                                        <div className="flex-column">
+                                            <Accordion type="single" collapsible className="w-full">
+
+                                                <AccordionItem value="org-security" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Organizational Security
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="op-security" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Operational Security
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                                <AccordionItem value="ai" className="border-lilac-800">
+                                                    <AccordionTrigger >
+                                                        Artificial Intelligence
+                                                    </AccordionTrigger>
+                                                    <AccordionContent className="text-black">
+                                                        Contenu...
+                                                    </AccordionContent>
+                                                </AccordionItem>
+
+                                            </Accordion>
+
+                                        </div>
+
+                                    </div>
+
+                                </DialogContent>
+
+                            </Dialog>
                         </div>
                     </div>
 
